f918d801587c730ee8d277bbd980c1bbe3e4db9c
Brendan Grainger <brendan.grainger@gmail.com>
2025-02-06 21:22:24 -0500

    added nested-form

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -13,6 +13,7 @@ class QuizzesController < ApplicationController
   # GET /quizzes/new
   def new
     @quiz = Quiz.new
+    @quiz.questions.build
   end
 
   # GET /quizzes/1/edit
@@ -65,6 +66,15 @@ class QuizzesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def quiz_params
-      params.require(:quiz).permit(:title)
+      params.require(:quiz).permit(
+        :title,
+        question_attributes: [
+          :id,
+          :_destroy,
+          :content,
+          :answer,
+          :image
+        ]
+      )
     end
 end
