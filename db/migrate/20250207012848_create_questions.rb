8353ab3987308bf6d3c00a554a6f5ee7e26b9f5c
Brendan Grainger <brendan.grainger@gmail.com>
2025-02-06 20:29:07 -0500

    added questions

---
+++ db/migrate/20250207012848_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[7.1]
+  def change
+    create_table :questions, id: :uuid do |t|
+      t.references :quiz, null: false, foreign_key: true, type: :uuid
+      t.text :content
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
